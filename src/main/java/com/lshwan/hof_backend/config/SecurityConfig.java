19a23d8fe0b3794a0c7b3592d76bedd89a3ff479
soung <sounghan94@gmail.com>
2025-01-26 22:24:14 +0900

    'swagger/jacoco완'

--- src/main/java/com/lshwan/hof_backend/config/SecurityConfig.java
+++ src/main/java/com/lshwan/hof_backend/config/SecurityConfig.java
@@ -1,10 +1,10 @@
-package com.lshwan.hof_backend.config;
+// package com.lshwan.hof_backend.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@Configuration
-@EnableMethodSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableMethodSecurity
+// public class SecurityConfig {
 
-}
+// }
