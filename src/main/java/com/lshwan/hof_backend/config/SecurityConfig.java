15363ed547ac62b6d1a1c4f954f895cdbd3eb902
soung <sounghan94@gmail.com>
2025-01-26 21:57:06 +0900

    'Security추가'

---
+++ src/main/java/com/lshwan/hof_backend/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.lshwan.hof_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+}
