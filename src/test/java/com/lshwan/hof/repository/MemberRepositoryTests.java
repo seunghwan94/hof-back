d238c42a2a69ad03d3359f1596559531922f6102
soung <sounghan94@gmail.com>
2025-02-09 01:40:16 +0900

    'cicdErrorTest2'

--- src/test/java/com/lshwan/hof/repository/MemberRepositoryTests.java
+++ src/test/java/com/lshwan/hof/repository/MemberRepositoryTests.java
@@ -1,20 +1,20 @@
-package com.lshwan.hof.repository;
+// package com.lshwan.hof.repository;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 
-import lombok.extern.log4j.Log4j2;
+// import lombok.extern.log4j.Log4j2;
 
-@SpringBootTest
-@Log4j2
-public class MemberRepositoryTests {
-  @Autowired
-  private MemberRepository repository;
+// @SpringBootTest
+// @Log4j2
+// public class MemberRepositoryTests {
+//   @Autowired
+//   private MemberRepository repository;
 
-  @Test
-  public void findAll(){
-    log.info(repository.findAll());
-  }
-}
+//   @Test
+//   public void findAll(){
+//     log.info(repository.findAll());
+//   }
+// }
