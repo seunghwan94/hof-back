2194ce7796efd10adbfd6ca432d1476935d7ce60
soung <sounghan94@gmail.com>
2025-02-10 22:36:46 +0900

    review

---
+++ src/main/java/com/lshwan/hof/domain/entity/order/Review.java
@@ -0,0 +1,52 @@
+package com.lshwan.hof.domain.entity.order;
+
+import com.lshwan.hof.domain.entity.BaseEntity;
+import com.lshwan.hof.domain.entity.member.Member;
+import com.lshwan.hof.domain.entity.prod.Prod;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_review")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no; 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pno", nullable = false)
+    private Prod prod;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mno", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_item_no", nullable = false)
+    private OrderItem orderItem;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false, columnDefinition = "int default 5")
+    private StarRating star = StarRating.FIVE; 
+
+    public enum StarRating {
+        ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5);
+        
+        private final int value;
+
+        StarRating(int value) {
+            this.value = value;
+        }
+    }
+}
