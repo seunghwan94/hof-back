9e6fcc0ec06d267271f6274bc71d8bea36936eaf
soung <sounghan94@gmail.com>
2025-02-10 23:11:33 +0900

    likes Entity

---
+++ src/main/java/com/lshwan/hof/domain/entity/common/Likes.java
@@ -0,0 +1,46 @@
+package com.lshwan.hof.domain.entity.common;
+
+import com.lshwan.hof.domain.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Table(name = "tbl_likes", uniqueConstraints = {
+    @UniqueConstraint(name = "tbl_likes_index_0", columnNames = {"mno", "target_no", "target_tpye"})
+})
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Likes extends BaseEntityRegDate {
+
+    @Embeddable
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LikesId implements Serializable {
+        private Long member;
+        private Long targetNo;
+        
+        @Enumerated(EnumType.STRING)
+        @Column(name = "target_tpye", nullable = false)
+        private TargetType targetType;
+    }
+
+    @EmbeddedId
+    private LikesId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("member")
+    @JoinColumn(name = "mno", nullable = false, foreignKey = @ForeignKey(name = "fk_likes_member"))
+    private Member member; 
+
+    public enum TargetType {
+        NOTE, PROD, REVIEW, FAV
+    }
+}
