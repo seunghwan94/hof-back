2a501cd9dd093db4bb2e8f8c59b67bd2276f09cf
soung <sounghan94@gmail.com>
2025-02-10 22:27:48 +0900

    payEntity

---
+++ src/main/java/com/lshwan/hof/domain/entity/payment/Pay.java
@@ -0,0 +1,45 @@
+package com.lshwan.hof.domain.entity.payment;
+
+import com.lshwan.hof.domain.entity.BaseEntity;
+import com.lshwan.hof.domain.entity.order.Order;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_pay")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pay extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_no", nullable = false)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "varchar(10) default '실패'")
+    private PaymentStatus status = PaymentStatus.실패;
+
+    @Column(nullable = false)
+    private int totalPrice;
+
+    public enum PaymentMethod {
+        카드, 이체, 무통장
+    }
+
+    public enum PaymentStatus {
+        완료, 실패
+    }
+}
