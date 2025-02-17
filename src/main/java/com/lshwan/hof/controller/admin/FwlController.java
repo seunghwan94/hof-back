76453cb358877b47cde892a630cecb4a24aea6df
kimyoungtae-kor <kyt4423@gmail.com>
2025-02-17 14:48:24 +0900

    FWL 완성및 자코코 테스트

---
+++ src/main/java/com/lshwan/hof/controller/admin/FwlController.java
@@ -0,0 +1,123 @@
+package com.lshwan.hof.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import com.lshwan.hof.domain.dto.PageRequestDto;
+import com.lshwan.hof.domain.dto.PageResultDto;
+import com.lshwan.hof.domain.dto.ProdDto;
+import com.lshwan.hof.domain.dto.SearchRequestDto;
+import com.lshwan.hof.domain.entity.admin.FWL;
+import com.lshwan.hof.domain.entity.member.Member;
+import com.lshwan.hof.domain.entity.prod.Prod;
+import com.lshwan.hof.service.MemberService;
+import com.lshwan.hof.service.admin.FwlService;
+import com.lshwan.hof.service.prod.ProdService;
+import com.lshwan.hof.service.util.SearchService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("admin/fwl")
+public class FwlController {
+  @Autowired
+  private MemberService service;
+  @Autowired
+  private FwlService fwlService;
+      @Autowired
+    private SearchService searchService;
+  @Autowired
+  private ProdService prodService;
+    
+
+  @GetMapping("/list")
+  public List<Member> listtest() {
+      return service.findList();
+  }
+  @PostMapping("/search")
+    public List<Map<String, Object>> search(@RequestBody SearchRequestDto request) {
+        return searchService.search(request);
+    }
+  @GetMapping
+  public List<FWL> fwlList() {
+    return fwlService.findList();
+  }
+  @PutMapping("/{fno}")
+  public ResponseEntity<?> putMethodName(@PathVariable("fno") Long fno, @RequestBody FWL entity) {
+
+    FWL existingFwl = fwlService.findBy(fno);
+
+    existingFwl.setIsActive(entity.getIsActive());
+
+
+    Long updatedId = fwlService.modify(existingFwl);
+
+    return ResponseEntity.ok().body("수정 완료: " + updatedId);
+  }
+  @DeleteMapping("/{ids}")
+
+  public ResponseEntity<?> deleteFwl(@PathVariable("ids") String ids) {
+      // "1,2,3" → [1, 2, 3] 변환
+      List<Long> fnoList = Arrays.stream(ids.split(","))
+                                .map(Long::parseLong)
+                                .collect(Collectors.toList());
+
+      if (fnoList.isEmpty()) {
+          return ResponseEntity.badRequest().body("삭제할 항목을 선택하세요.");
+      }
+
+      int deleteCount = 0;
+      for (Long fno : fnoList) {
+          if (fwlService.remove(fno)) {
+              System.out.println("삭제된 fno: " + fno);
+              deleteCount++;
+          }
+      }
+      return ResponseEntity.ok().body("삭제 완료: " + deleteCount + "개 항목");
+  }
+
+  
+  @PostMapping
+  public ResponseEntity<?> addFwl(@RequestBody FWL entity) {
+    Long newId = fwlService.add(entity);
+     return ResponseEntity.ok().body("등록 완료: " + newId);
+    // return ResponseEntity.badRequest().body(entity);
+}
+  
+/////////////////////////prod
+@GetMapping("/prod")
+    public ResponseEntity<PageResultDto<ProdDto, Prod>> getProdList(@RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(required = false) String type,
+        @RequestParam(required = false) String keyword) {
+        PageRequestDto dto = PageRequestDto.builder()
+        .page(page)
+        .size(size)
+        .type(type)
+        .keyword(keyword)
+        .build();
+        return ResponseEntity.ok().body(prodService.findList(dto));
+    }
+  
+}
---
+++ src/test/java/com/lshwan/hof/controller/FwlControllerTests.java
@@ -0,0 +1,94 @@
+package com.lshwan.hof.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+public class FwlControllerTests {
+   private MockMvc mockMvc;
+
+   // 각각의 테스트 실행전 초기화
+  @BeforeEach
+  public void init(WebApplicationContext ctx) {
+    mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+  }
+
+  @Test
+  public void testGetAllFWL() throws Exception {
+    // GET
+    mockMvc.perform(get("/admin/fwl")) 
+      // 응답 상태
+      .andExpect(status().isOk()) 
+      // 응답json인지 확인
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)) 
+    // 테스트 결과 출력
+    .andDo(print()); 
+  }
+
+  @Test
+  @Transactional
+  public void testinsertFWL() throws Exception {
+     String jsonContent = new ObjectMapper().writeValueAsString(Map.of("content", "침대zzzx"));
+    mockMvc.perform(post("/admin/fwl")
+      .contentType(MediaType.APPLICATION_JSON)
+      // Query String 파라미터
+      .content(jsonContent))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType("text/plain;charset=UTF-8"))
+      // .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+      .andDo(print());
+  }
+
+  @Test
+  @Transactional
+  public void testUpdateFWL() throws Exception {
+    int fno = 50;
+    String jsonContent = new ObjectMapper().writeValueAsString(Map.of("content", "침대zzzvv"));
+    mockMvc.perform(put("/admin/fwl/"+fno)
+    .contentType(MediaType.APPLICATION_JSON)
+      .content(jsonContent))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType("text/plain;charset=UTF-8"))
+      // .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+    .andDo(print());
+  }
+
+  @Test
+  @Transactional
+  public void testDeleteFWL() throws Exception {
+    Long pno = 50L;
+
+    mockMvc.perform(delete("/admin/fwl/"+ pno))
+      .andExpect(status().isOk()) // 200 OK 확인
+      .andExpect(content().contentType("text/plain;charset=UTF-8"))
+      // .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+      // .andExpect(jsonPath("$.pno").value(pno)) // 응답 JSON에서 pno 값이 일치하는지 확인
+      // .andExpect(jsonPath("$.title").exists()) // title 필드가 존재하는지 확인
+      // .andExpect(jsonPath("$.content").exists()) // content 필드가 존재하는지 확인
+    .andDo(print());
+  }
+}
