7303a564cb794a19ee4b276df7cfaa464afaa499
soung <sounghan94@gmail.com>
2025-03-04 23:02:53 +0900

    'swagger배포2'

--- src/main/java/com/lshwan/hof/config/SwaggerConfig.java
+++ src/main/java/com/lshwan/hof/config/SwaggerConfig.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @OpenAPIDefinition( servers = {
-            @Server(url = "https://hof.lshwan.com", description = "배포 서버"),
-            @Server(url = "http://localhost:8080", description = "로컬 서버")
+            @Server(url = "https://hof.lshwan.com/api/v1", description = "배포 서버"),
+            @Server(url = "http://localhost:8080/api/v1", description = "로컬 서버")
         },
         info = @Info(title = "가구의집 API 공식문서",version = "1.0",description = "Spring/React spring boot ver 3.4.2.SnapShot"))
 public class SwaggerConfig {
